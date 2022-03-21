f96a0922e35217e47106cc98fb68cbd311809dd4
Leonardo IGZ <leoguz079@hotmail.com>
2022-03-20 22:41:33 -0600

    Se añade el dibujado de una ruta apartir de puntos estaticos, además de implemenatar el borrado de la ruta si así se desea

--- app/src/main/java/com/ligz/routedrawing/MainActivity.java
+++ app/src/main/java/com/ligz/routedrawing/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.Context;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.util.Log;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.android.volley.RequestQueue;
@@ -43,6 +44,7 @@ public class MainActivity extends AppCompatActivity {
     private RequestQueue queue;
     private JsonObjectRequest requestMapRequest;
     private CompassOverlay mCompassOverlay;
+    private Button btnPosition, btnDeletePosition;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -67,18 +69,18 @@ public class MainActivity extends AppCompatActivity {
 
         map.setMultiTouchControls(true);
 
-        this.mLocationOverlay = new MyLocationNewOverlay(new GpsMyLocationProvider(ctx),map);
+        this.mLocationOverlay = new MyLocationNewOverlay(new GpsMyLocationProvider(ctx), map);
         this.mLocationOverlay.enableMyLocation();
         map.getOverlays().add(this.mLocationOverlay);
 
-        this.mCompassOverlay = new CompassOverlay(ctx, new InternalCompassOrientationProvider(ctx),map);
+        this.mCompassOverlay = new CompassOverlay(ctx, new InternalCompassOrientationProvider(ctx), map);
         this.mCompassOverlay.enableCompass();
         map.getOverlays().add(this.mCompassOverlay);
 
         IMapController mapController = map.getController();
         mapController.setZoom(19.0);
 
-        GeoPoint startPoint = new GeoPoint(20.139476, -101.150737);
+        GeoPoint startPoint = new GeoPoint(20.14649016556056,-101.17566401392817);
         mapController.setCenter(startPoint);
 
         this.mLocationOverlay = new MyLocationNewOverlay(new GpsMyLocationProvider(ctx), map);
@@ -91,8 +93,7 @@ public class MainActivity extends AppCompatActivity {
 
         //your items
         ArrayList<OverlayItem> items = new ArrayList<OverlayItem>();
-        items.add(new OverlayItem("Title", "Description", new GeoPoint(20.139476d,
-                -101.150737d))); // Lat/Lon decimal degrees
+        items.add(new OverlayItem("Title", "Description", new GeoPoint(20.14649016556056,-101.17566401392817))); // Lat/Lon decimal degrees
 
         //the overlay
         ItemizedOverlayWithFocus<OverlayItem> mOverlay = new ItemizedOverlayWithFocus<OverlayItem>(items,
@@ -119,8 +120,18 @@ public class MainActivity extends AppCompatActivity {
         startMarker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM);
         map.getOverlays().add(startMarker);
 
+        btnPosition = findViewById(R.id.btnPos);
+        btnDeletePosition = findViewById(R.id.btnDelete);
 
+        btnPosition.setOnClickListener(view -> {
+            GeoPoint geoPoint = new GeoPoint(20.14649016556056,-101.17566401392817);
+            mapController.setCenter(geoPoint);
+            obtenerRouteFromMapRequest();
+        });
 
+        btnDeletePosition.setOnClickListener(view -> {
+            map.getOverlays().clear();
+        });
     }
 
     @Override
@@ -144,6 +155,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void obtenerRouteFromMapRequest() {
+        List<GeoPoint> puntosRuta = new ArrayList<>();
         queue =
                 Volley.newRequestQueue(this);
 
@@ -167,9 +179,16 @@ public class MainActivity extends AppCompatActivity {
                                                 + "," +
                                                 indi.getJSONObject("startPoint").get("lng").toString();
 
+
+
                                         Log.d("GIVO", "se ejecuto: " + strlatlog);
+                                        puntosRuta.add(new GeoPoint(Double.parseDouble(indi.getJSONObject("startPoint").get("lat").toString()),
+                                                Double.parseDouble(indi.getJSONObject("startPoint").get("lng").toString())));
 
                                     }
+
+                                    dibujarPolilinea(puntosRuta);
+
                                 } catch (JSONException e) {
                                     e.printStackTrace();
                                 }
@@ -185,5 +204,26 @@ public class MainActivity extends AppCompatActivity {
                 );
 
         queue.add(requestMapRequest);
+        Log.d("GIVO", "se ejecuto antes que los puntos");
+    }
+
+    private void dibujarPolilinea(List<GeoPoint> geoPoints) {
+
+        Polyline line = new Polyline();   //see note below!
+        line.setPoints(geoPoints);
+        line.setOnClickListener(new Polyline.OnClickListener() {
+            @Override
+            public boolean onClick(Polyline polyline, MapView mapView, GeoPoint eventPos) {
+                Toast.makeText(mapView.getContext(), "polyline with " + polyline.getPoints().size() + "pts was tapped", Toast.LENGTH_LONG).show();
+                return false;
+            }
+        });
+        map.getOverlayManager().add(line);
+
+        IMapController mapController = map.getController();
+        mapController.setZoom(17.0);
+        GeoPoint startPoint = new GeoPoint(20.14649016556056,-101.17566401392817);
+        mapController.setCenter(startPoint);
+
     }
 }
